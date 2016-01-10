b254dbff2f7388150b8bd9e38dfe1cb0ea637c51
superbeef150 <superbeef150@gmail.com>
2016-01-10 15:04:38 -0500

    Update Display on Coin Insert Tests begun. Also updated IDE to autosave to prevent further blank checkins

--- vendingMachineTests.js
+++ vendingMachineTests.js
@@ -3,15 +3,20 @@ describe("vendingMachine", function(){
   
   it("should accept coins so that money can be collected from the customer", function () {
     var result = vendingMachine.insertCoin(5); //nickels are just fine
-    expect(result).toEqual(successCode);
+    expect(result).toEqual(successCode); //todo - insertCoin return updated display on success?
   });
-  //todo - add additional 
+  //todo - add additional coin acceptance tests
   
   it("should reject invalid currency", function(){
-    var result = vendinmachine.insertCoin(1); //pennies are no bueno. They cost more to mint than they're worth anyway lol
-    expect(result).toEqual(failureCode);
+    var result = vendingMachine.insertCoin(1); //pennies are no bueno. They cost more to mint than they're worth anyway lol
+    expect(result).toEqual(failureCode); //todo - insertCoin return failureCode (negative number?) on failure?
+  });
+  //todo - add additional coin failure tests
+  
+  it("should add to the total when a valid coin is inserted", function(){
+    var result = vendingMachine.insertCoin(25);
+    expect(result).toEqual(successCode); //todo - insertCoin return updated display on success?
   });
   
-  
-  
+  //todo - test to assert display says INSERT COIN when no coins are inserted
 });
