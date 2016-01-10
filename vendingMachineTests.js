f1efbd833a59e1bb23929b1349c3382b90b9cffd
superbeef150 <superbeef150@gmail.com>
2016-01-10 14:38:07 -0500

    Invalid currency tests begun

--- vendingMachineTests.js
+++ vendingMachineTests.js
@@ -3,7 +3,12 @@ describe("vendingMachine", function(){
   
   it("should accept coins so that money can be collected from the customer", function () {
     var result = vendingMachine.insertCoin(5);
-    expect(result).toEqual(true);
+    expect(result).toEqual(successCode);
+  });
+  
+  it("should reject invalid currency", function(){
+    var result = vendinmachine.insertCoin(1);
+    expect(result).toEqual(failureCode);
   });
   
 });
