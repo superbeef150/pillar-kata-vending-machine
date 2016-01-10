30bef459f449bad2deeca6628837900182b978a5
superbeef150 <superbeef150@gmail.com>
2016-01-10 15:00:50 -0500

    Update Display on Coin Insert Tests begun

--- vendingMachineTests.js
+++ vendingMachineTests.js
@@ -2,13 +2,16 @@
 describe("vendingMachine", function(){
   
   it("should accept coins so that money can be collected from the customer", function () {
-    var result = vendingMachine.insertCoin(5);
+    var result = vendingMachine.insertCoin(5); //nickels are just fine
     expect(result).toEqual(successCode);
   });
+  //todo - add additional 
   
   it("should reject invalid currency", function(){
-    var result = vendinmachine.insertCoin(1);
+    var result = vendinmachine.insertCoin(1); //pennies are no bueno. They cost more to mint than they're worth anyway lol
     expect(result).toEqual(failureCode);
   });
+  
+  
   
 });
