00d0ae42cab8338bf7380f5cd563793d829b7590
superbeef150 <superbeef150@gmail.com>
2016-01-11 06:46:55 -0500

    Added clearCoins method

--- vendingmachine.js
+++ vendingmachine.js
@@ -6,9 +6,13 @@ var vendingMachine = function () {
     if (coin === 5 || coin === 10 || coin === 25){ //todo - put acceptable coins into a constant list and refactor this line using that
       coinsCurrentlyInserted += coin;
     }
-    return -1; //failure (bad coin?) //todo - log failure to console?
+    return -1; //failure (bad coin?) //todo - log failure to console? //todo - make -1 a constant
   }
   
+  var clearCoins = function (){
+    coinsCurrentlyInserted = 0;
+    return 1; //todo - make 1 a constant (successCode?)
+  }
   
   
 }();
